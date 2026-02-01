f348068d0767600de3d5c4064ef1ccfdf8842f09
prasant <rayandhamala1999@gamil.com>
2026-02-02 08:21:53 +1100

    Implemented image history and undo functionality in PicModel

--- pic_model.py
+++ pic_model.py
@@ -4,6 +4,8 @@ import os
 class PicModel:
     def __init__(self):
         self._presentValue = None
+        self._previousList = []
+        self._redoList = []
         self._filename = ""
 
     def load(self, path):
@@ -15,3 +17,19 @@ class PicModel:
 
         self._presentValue = img
         self._filename = os.path.basename(path)
+
+        # Save initial state for undo
+        self._previousList = [img.copy()]
+        self._redoList.clear()
+
+    def addState(self):
+        if self._presentValue is not None:
+            self._previousList.append(self._presentValue.copy())
+            self._redoList.clear()
+
+    def undo(self):
+        if len(self._previousList) > 1:
+            self._redoList.append(self._previousList.pop())
+            self._presentValue = self._previousList[-1].copy()
+            return True
+        return False
